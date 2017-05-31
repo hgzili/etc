b02ee6708678361183359a8414aa1791cba66910
hgzili <604148273@qq.com>
2017-05-31 14:51:50 +0800

    Update etcTest.java
    test

--- src/test/java/etc/etcTest.java
+++ src/test/java/etc/etcTest.java
@@ -16,7 +16,7 @@ public class etcTest extends AbstractTransactionalJUnit4SpringContextTests{
 	@Test
 	public void testGetTicker() {
 		EtcUtil.getDepth();
-		//2
+		//3
 //		TickerTable tickerTable = EtcUtil.getTicker();
 //		etcService.saveOrUpdateTickerTable(tickerTable);
 	}
